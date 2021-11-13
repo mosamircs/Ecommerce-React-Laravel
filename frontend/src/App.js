0c2651b35ba3a8c4eadf28d3fa7fa1e033eca261
Mohamed Samir <mosamircs@gamil.com>
2021-11-13 19:37:19 +0200

    front end part using Reactjs

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ecommerce</h1>
+        <button>simple button</button>
+        <Button>Bootstrap button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
